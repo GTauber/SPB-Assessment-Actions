da2665064fcc8d82823584a19790536ff249da4b
Gabriel Tauber <gabri.tauber@gmail.com>
2024-09-27 21:45:55 -0300

    feat: Trigger git actions

---
+++ src/main/java/assessment/infnet/actions/controller/testController.java
@@ -0,0 +1,17 @@
+package assessment.infnet.actions.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class testController {
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Teste");
+    }
+
+}
